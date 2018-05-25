f8c58f8285f5f65cb5a59cc15eae5bb5f851ad7a
Hosung Song <hoson@microsoft.com>
2018-05-25 15:53:41 -0700

    Reflect Azure CLI repo signing key change (fixes #134)

--- scripts/install_moodle.sh
+++ scripts/install_moodle.sh
@@ -149,7 +149,7 @@
         echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
             sudo tee /etc/apt/sources.list.d/azure-cli.list
 
-        sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893 >> /tmp/apt4.log
+        curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add - >> /tmp/apt4.log
         sudo apt-get -y install apt-transport-https >> /tmp/apt4.log
         sudo apt-get -y update > /dev/null
         sudo apt-get -y install azure-cli >> /tmp/apt4.log
