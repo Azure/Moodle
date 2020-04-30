1cb6f239a68f62e56d192889b64f8d4e377e0118
unknown <asift@italentdigital.com>
2020-04-30 09:27:03 +0530

    Dropping TLSv1 & TLSv1.1 and keeping TLSv1.2

--- scripts/setup_webserver.sh
+++ scripts/setup_webserver.sh
@@ -155,7 +155,8 @@ http {
 
   set_real_ip_from   127.0.0.1;
   real_ip_header      X-Forwarded-For;
-  ssl_protocols TLSv1 TLSv1.1 TLSv1.2; # Dropping SSLv3, ref: POODLE
+  # Dropping TLSv1 TLSv1.1 
+  ssl_protocols TLSv1.2; # Dropping SSLv3, ref: POODLE
   ssl_prefer_server_ciphers on;
 
   gzip on;
