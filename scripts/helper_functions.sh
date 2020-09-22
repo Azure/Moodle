bb561edd8b88841edb7e3f16f9d6dc1b9854b856
Asif iTalent <60592188+asift91@users.noreply.github.com>
2020-09-22 21:47:43 +0530

    Update helper_functions.sh

--- scripts/helper_functions.sh
+++ scripts/helper_functions.sh
@@ -8,10 +8,10 @@ function get_setup_params_from_configs_json
 
     # (dpkg -l jq &> /dev/null) || (apt -y update; apt -y install jq)
     # sudo add-apt-repository universe
-	  # sudo apt-get -y update
-	  # sudo apt-get -y install jq
-    JQ=/usr/bin/jq
-    curl https://stedolan.github.io/jq/download/linux64/jq > $JQ && chmod +x $JQ
+    # sudo apt-get -y update
+    # sudo apt-get -y install jq
+
+    curl https://stedolan.github.io/jq/download/linux64/jq > /usr/bin/jq && chmod +x /usr/bin/jq
 	
     # Wait for the cloud-init write-files user data file to be generated (just in case)
     local wait_time_sec=0
